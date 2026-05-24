ca8b87851dafce8b5b7eb39cda933eb5ae9a0c56
slopero <v.a.krasnov2005@mail.ru>
2026-05-24 13:24:55 +0700

    СОздание таблиц перед запуском

--- backend/script.py
+++ backend/script.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from routers import books
+from data.database import create_tables
+from contextlib import asynccontextmanager
+import models.models
 
 
-app = FastAPI(title="Онлайн-библиотека Идея")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()  # Создаём таблицы при старте приложения
+    yield  # Здесь будет выполняться обработка запросов
+
+app = FastAPI(title="Онлайн-библиотека Идея", lifespan=lifespan)
 app.include_router(books.router)
+
+
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
